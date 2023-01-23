0ef3757ec4a3591d4f49b53f8a2d76ea633e8b3a
Clarence <clarencema@live.com>
2023-01-23 15:44:29 -0800

    reviews migration,model, and seeds complete

---
+++ backend/db/models/review.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Review.belongsTo(models.Spot, { foreignKey: 'spotId' });
+      Review.belongsTo(models.User, { foreignKey: 'userId' });
+    }
+  }
+  Review.init({
+    spotId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
+    review: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    stars: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 1,
+        max: 5
+      }
+    },
+  }, {
+    sequelize,
+    modelName: 'Review',
+  });
+  return Review;
+};
---
+++ backend/db/seeders/20230123233649-demo-reviews.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    options.tableName = 'Reviews';
+    return queryInterface.bulkInsert(options, [
+      {
+        spotId: 1,
+        userId: 3,
+        review: 'This place was great',
+        stars: 4
+      },
+      {
+        spotId: 3,
+        userId: 1,
+        review: 'It was okay',
+        stars: 3
+      },
+      {
+        spotId: 2,
+        userId: 2,
+        review: 'I liked this place',
+        stars: 5
+      }
+    ], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Reviews';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      userId: { [Op.in]: [1, 2, 3] }
+    }, {})
+  }
+};
