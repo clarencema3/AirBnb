cfd87174dd042d087df987dd4484e8af8ad200bf
Clarence <clarencema@live.com>
2023-01-23 15:18:45 -0800

    spots migrations,tables,and seeders complete

--- backend/db/migrations/20230123204327-create-booking.js
+++ backend/db/migrations/20230123204327-create-booking.js
@@ -16,7 +16,11 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       spotId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Spots'
+        },
+        onDelete: "CASCADE"
       },
       userId: {
         type: Sequelize.INTEGER,
