171544e51e2662b0a83ad86ae986893b7d3d64a2
Clarence <clarencema@live.com>
2023-01-23 16:26:44 -0800

    spotimages seeders created

---
+++ backend/db/seeders/20230124002238-demo-spotImages.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    options.tableName = "SpotImages";
+    return queryInterface.bulkInsert(options, [
+      {
+        spotId: 1,
+        url: "https://unsplash.com/photos/bSpVH5V0YX8",
+        preview: true
+      },
+      {
+        spotId: 2,
+        url: "https://unsplash.com/photos/nWMcDsDAxu0",
+        preview: false
+      }
+    ], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = "SpotImages";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      url: { [Op.in]: ["https://unsplash.com/photos/bSpVH5V0YX8", "https://unsplash.com/photos/nWMcDsDAxu0"]}
+    }, {});
+  }
+};
