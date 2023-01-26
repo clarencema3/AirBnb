50a3a627b26753cb6781d0496b3e2033e9d828b1
Clarence <clarencema@live.com>
2023-01-26 10:17:39 -0800

    delete spot image

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js')
 const bookingsRouter = require('./bookings.js');
+const spotImagesRouter = require('./spot-images.js');
 const { restoreUser } = require('../../utils/auth.js');
 router.use(restoreUser);
 
@@ -12,6 +13,7 @@ router.use('/session', sessionRouter);
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 router.use('/bookings', bookingsRouter);
+router.use('/spot-images', spotImagesRouter);
 // router.post('/test', function(req, res) {
 //     res.json({ requestBody: req.body });
 // });
